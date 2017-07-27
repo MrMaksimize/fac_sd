515e1bcf503136c7d50ebadd774f05fc8eadf243
Maksim Pecherskiy <max@maksimize.com>
2017-07-27 11:20:45 -0700

    Adding mapbox-gl.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -15,6 +15,7 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    "/bower_components/font-awesome/fonts/**"
   ],
   navigateFallback: 'index.html',
 };
